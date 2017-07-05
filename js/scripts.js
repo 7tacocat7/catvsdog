a62efc16ee7e04d7d7b791c5351565a53d6edb48
Carson Canales and Kate Mashokha <carson@>
2017-07-05 15:07:45 -0700

    added final steps

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,15 @@ $(document).ready(function(){
   });
 });
 
+  $("button#bark").click(function(){
+    $("ul#dog").prepend("<li>Bark!</li>");
+    $("ul#cat").prepend("<li>meow</li>");
+
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+});
 });
